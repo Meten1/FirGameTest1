b0e88f67702ee3f761b83845e318500c3b311e72
Meten1 <xiangyu.huo@my.jcu.edu.au>
2023-03-13 01:34:01 +0800

    Add a login interface for the game, change the main activity to the login interface, and add the database and data insertion functions in the login interface, and set the game as a separate activity.

---
+++ app/src/main/java/au/edu/jcu/firgametest/LoginSQLHelper.java
@@ -0,0 +1,34 @@
+package au.edu.jcu.firgametest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class LoginSQLHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "user.db";
+    private static final int DB_VERSION = 1;
+
+    public LoginSQLHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // 创建数据库表
+        String createTableSql = "CREATE TABLE IF NOT EXISTS user " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "name VARCHAR(10), " +
+                "password VARCHAR(15)," +
+                "score INT" +
+                ")";
+        db.execSQL(createTableSql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // 升级数据库表
+        String dropTableSql = "DROP TABLE IF EXISTS user";
+        db.execSQL(dropTableSql);
+        onCreate(db);
+    }
+}
